5be0ce5d116abf571c85ab2e6655c63ce5ad4a55
Dan <daweidern@gmail.com>
2022-07-02 15:08:01 -0500

    commit tree cc6367bdbd7f4ca94a6dacbab36770a4145eb1ae

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+// first deliverable
+const returnFirstTwoDrivers = function(staff){
+    const firstDrivers = staff.slice(0,2);
+        return firstDrivers;
+}
+// second deliverable
+const returnLastTwoDrivers = function(staff){
+    const lastDrivers = staff.slice(2,4);
+        return lastDrivers;
+}
+// third deliverable
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+// fourth deliverable
+function createFareMultiplier(fare) {
+    const multiplier = function (){
+        return fare * fare;
+    }
+        return multiplier;
+}
+
+// fifth deliverable
+function fareDoubler(fare){
+    const doubler = fare * 2;
+        return doubler;
+}
+
+// sixth deliverable
+function fareTripler(fare){
+    const tripler = fare * 3;
+        return tripler;
+}
+// seventh deliverable
+function selectDifferentDrivers(drivers,returnLastTwoDrivers){
+        return returnLastTwoDrivers(drivers);
+}
+
+// complete 
